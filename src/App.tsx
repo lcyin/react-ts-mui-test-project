b1e00d72ba279a958408fec0a04a30c25bc5aec8
lcyin <laichunyinkevin@gmail.com>
2022-02-12 15:01:33 +0800

    deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import RouteInfo from './components/RouteInfo';
+import Home from '../components/Home';
+import RouteInfo from '../components/RouteInfo';
 
 import './style.css';
 

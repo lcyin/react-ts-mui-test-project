df0c4435c4178d71d594e2abcc5f05687e5310bc
lcyin <laichunyinkevin@gmail.com>
2022-02-12 14:43:54 +0800

    1 file modified

--- hooks/useHomeFetch.ts
+++ hooks/useHomeFetch.ts
@@ -6,16 +6,17 @@ export const useHomeFetch = () => {
   useEffect(() => {
     const getAllRoute = async () => {
       const allRoute = await API.getAllRoute();
-      const routeDataRow = allRoute.data.map((data, id) => {
-        return {
-          id,
-          route: data.route,
-          title: `往: ${data.dest_tc} `,
-          mins: `- 分鐘`,
-          bound: data.bound,
-        };
-      });
-      // .filter((data) => data.route === '296C');
+      const routeDataRow = allRoute.data
+        .map((data, id) => {
+          return {
+            id,
+            route: data.route,
+            title: `往: ${data.dest_tc} `,
+            mins: `- 分鐘`,
+            bound: data.bound,
+          };
+        })
+        .filter((data) => data.route === '296C');
       setDataRow(routeDataRow);
     };
     getAllRoute();
